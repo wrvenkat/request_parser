f09e2e01bd0e6cb530b4069522e6ae148851c58a
Venkat Raman <wrvenkat@gmail.com>
2019-03-10 19:20:11 -0400

    1. Added some settings change for uploadhandlers and max in-memory size. 2. Fixed some super() calls to Python 2.x in uploadhandler.py.

--- request_parser/conf/settings.py
+++ request_parser/conf/settings.py
@@ -11,8 +11,20 @@ DEFAULT_CHARSET = 'utf-8'
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 4096
 
-DATA_UPLOAD_MAX_MEMORY_SIZE = 1024
+#max memory size reserved for in-memory file handling
+#default is 50 MB
+DATA_UPLOAD_MAX_MEMORY_SIZE = 50 * ((2 ** 10) * (2 ** 10))
 
+#flag that configures whether request-parser should treat requests
+#as if they're meant for a web-proxy
 WEB_PROXY = False
 
-FILE_UPLOAD_HANDLERS = { "TODO: FIX ME" }
+#holds the different upload handlers
+#the ones listed below are the default ones which Django/request-parser
+#requires to work properly while others can be added
+FILE_UPLOAD_HANDLERS = [
+    #hold uploaded files in-memory
+    'request_parser.files.uploadhandler.MemoryFileUploadHandler',
+    #hold uploaded files in a temp file
+    'request_parser.files.uploadhandler.TemporaryFileUploadHandler',
+ ]
--- request_parser/files/uploadhandler.py
+++ request_parser/files/uploadhandler.py
@@ -136,7 +136,7 @@ class TemporaryFileUploadHandler(FileUploadHandler):
         """
         Create the file object to append to as data is coming in.
         """
-        super().new_file(*args, **kwargs)
+        super(TemporaryFileUploadHandler, self).new_file(*args, **kwargs)
         self.file = TemporaryUploadedFile(self.file_name, self.content_type, 0, self.charset, self.content_type_extra)
 
     def receive_data_chunk(self, raw_data, start):
