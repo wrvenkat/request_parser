6fe5795b00bb77b6949ab73587da0ccb977d0f5e
Venkat Raman <wrvenkat@gmail.com>
2019-03-19 21:24:23 -0400

    Fixed uses_params dependency on six. Now uses_params is from future.

--- request_parser/utils/http.py
+++ request_parser/utils/http.py
@@ -17,13 +17,9 @@ from urllib import (
 from urlparse import (
     ParseResult, SplitResult, scheme_chars
 )
-import six.moves.urllib.parse as url_parse
-#from six.moves.urllib import (
-#    _coerce_args, _splitnetloc, _splitparams
-#)
 
 from future.backports.urllib.parse import (
-    _coerce_args, _splitnetloc, _splitparams
+    _coerce_args, _splitnetloc, _splitparams, uses_params
 )
 
 from request_parser.exceptions.exceptions import TooManyFieldsSent
@@ -322,7 +318,7 @@ def _urlparse(url, scheme='', allow_fragments=True):
     url, scheme, _coerce_result = _coerce_args(url, scheme)
     splitresult = _urlsplit(url, scheme, allow_fragments)
     scheme, netloc, url, query, fragment = splitresult    
-    if scheme in url_parse.uses_params and ';' in url:
+    if scheme in uses_params and ';' in url:
         url, params = _splitparams(url)
     else:
         params = ''
