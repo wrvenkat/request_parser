639c07304d270e4015923a5223a9c0d12be29155
Venkat Raman <wrvenkat@gmail.com>
2019-03-24 18:43:51 -0400

    Changed a few default max data size and added some comments to settings.py

--- request_parser/conf/settings.py
+++ request_parser/conf/settings.py
@@ -2,9 +2,10 @@
 class Settings:
     """Setting used to configure the parser"""
 
-
+#Directory where file upload files will be stored
 FILE_UPLOAD_TEMP_DIR = '.'
 
+#Default max size of uploaded file is 80MB
 FILE_UPLOAD_MAX_MEMORY_SIZE = 80 * ((2 ** 10) * (2 ** 10))
 
 DEFAULT_CHARSET = 'utf-8'
@@ -12,10 +13,10 @@ DEFAULT_CHARSET = 'utf-8'
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 4096
 
 #max memory size reserved for in-memory file handling
-#default is 50 MB
-DATA_UPLOAD_MAX_MEMORY_SIZE = 50 * ((2 ** 10) * (2 ** 10))
+#default is 100 MB
+DATA_UPLOAD_MAX_MEMORY_SIZE = 100 * ((2 ** 10) * (2 ** 10))
 
-#flag that configures whether request-parser should treat requests
+#Flag that configures whether request-parser should treat requests
 #as if they're meant for a web-proxy
 WEB_PROXY = False
 
