8142a30660cd6fad3de6305e1c08fe99b323e284
Venkat Raman <wrvenkat@gmail.com>
2019-04-22 08:40:36 -0400

    Fixed Settings class formatting and added a TODO.

--- request_parser/conf/settings.py
+++ request_parser/conf/settings.py
@@ -2,33 +2,35 @@
 class Settings:
     """Setting used to configure the parser"""
 
-#Directory where file upload files will be stored
-FILE_UPLOAD_TEMP_DIR = '.'
+    #TODO: Make this configurable to be an object
 
-#max header size of a header
-MAX_HEADER_SIZE = 16
+    #Directory where file upload files will be stored
+    FILE_UPLOAD_TEMP_DIR = '.'
 
-#Default max size of uploaded file is 80MB
-FILE_UPLOAD_MAX_MEMORY_SIZE = 80 * ((2 ** 10) * (2 ** 10))
+    #max header size of a header
+    MAX_HEADER_SIZE = 16
 
-DEFAULT_CHARSET = 'utf-8'
+    #Default max size of uploaded file is 80MB
+    FILE_UPLOAD_MAX_MEMORY_SIZE = 80 * ((2 ** 10) * (2 ** 10))
 
-DATA_UPLOAD_MAX_NUMBER_FIELDS = 4096
+    DEFAULT_CHARSET = 'utf-8'
 
-#max memory size reserved for in-memory file handling
-#default is 100 MB
-DATA_UPLOAD_MAX_MEMORY_SIZE = 100 * ((2 ** 10) * (2 ** 10))
+    DATA_UPLOAD_MAX_NUMBER_FIELDS = 4096
 
-#Flag that configures whether request-parser should treat requests
-#as if they're meant for a web-proxy
-WEB_PROXY = False
+    #max memory size reserved for in-memory file handling
+    #default is 100 MB
+    DATA_UPLOAD_MAX_MEMORY_SIZE = 100 * ((2 ** 10) * (2 ** 10))
 
-#holds the different upload handlers
-#the ones listed below are the default ones which Django/request-parser
-#requires to work properly while others can be added
-FILE_UPLOAD_HANDLERS = [
-    #hold uploaded files in-memory
-    'request_parser.files.uploadhandler.MemoryFileUploadHandler',
-    #hold uploaded files in a temp file
-    'request_parser.files.uploadhandler.TemporaryFileUploadHandler',
- ]
+    #Flag that configures whether request-parser should treat requests
+    #as if they're meant for a web-proxy
+    WEB_PROXY = False
+
+    #holds the different upload handlers
+    #the ones listed below are the default ones which Django/request-parser
+    #requires to work properly while others can be added
+    FILE_UPLOAD_HANDLERS = [
+        #hold uploaded files in-memory
+        'request_parser.files.uploadhandler.MemoryFileUploadHandler',
+        #hold uploaded files in a temp file
+        'request_parser.files.uploadhandler.TemporaryFileUploadHandler',
+    ]
